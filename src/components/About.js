3f91e874b710b52577a2ab1737f2fd6e892e5dee
Nicole Baird <nbaird44@gmail.com>
2022-01-31 13:09:11 -0500

    about component styling

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+function About () {
+    return (
+        <div className="about">
+            <div className="about-image">
+                <img className="trees-icon" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643652000/my-sustainable-brand/kindpng_75268_mepm63.png" alt="trees" />
+            </div>
+            <div className="about-text">
+                <h2 className="about-headline">OUR MISSION.</h2>
+                <p className="about-text">
+                ladjdska dfjafldkj aldkfjaldkd alkdfjlsdka akdfalkdfadlkjfalkd jadlkfadlk jfoiaebdo djoiafhobn adif hdfiohadofihadfohadifhjoafidjaoijdfoifjd ladjdska dfjafldkj aldkfjaldkd alkdfjlsdka akdfalkdfadlkjfalkd jadlkfadlk jfoiaebdo djoiafhobna dif hdfiohadofihadfohadifhjoafidjaoijdfoifjd
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 function Home(props) {
     return (
         <div>
            <Hero />
+           <About />
         </div>
     )
 }
