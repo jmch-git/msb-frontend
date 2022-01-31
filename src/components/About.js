6a2a58d08ce1b2054e002079431af0c29bdde19b
Nicole Baird <nbaird44@gmail.com>
2022-01-31 13:46:22 -0500

    ImageText component complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ function About () {
     return (
         <div className="about">
             <div className="about-image">
-                <img className="trees-icon" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643652000/my-sustainable-brand/kindpng_75268_mepm63.png" alt="trees" />
+                <img className="trees-icon" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643652878/my-sustainable-brand/favpng_pine-tree-fir-clip-art_wua9dk.png" alt="trees" />
             </div>
             <div className="about-text">
                 <h2 className="about-headline">OUR MISSION.</h2>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import About from "../components/About";
+import ImageText from "../components/ImageText";
 
 function Home(props) {
     return (
         <div>
            <Hero />
            <About />
+           <ImageText />
         </div>
     )
 }
