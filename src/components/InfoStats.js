085619e9f79cabc44953dd562359840a32484673
Nicole Baird <nbaird44@gmail.com>
2022-02-02 09:30:39 -0500

    tips content complete

--- src/components/InfoStats.js
+++ src/components/InfoStats.js
@@ -2,16 +2,16 @@ function InfoStats() {
     return (
         <div className="info-stats">
             <div className="stat-one">
-                <p className="stat-one-number">24</p>
-                <p className="stat-text">DATA POINT NUMBER ONE</p>
+                <p className="stat-one-number">20%</p>
+                <p className="stat-text">of the wastewater world wide is attributed to fashion industry</p>
             </div>
             <div className="stat-two">
-                <p className="stat-two-number">200</p>
-                <p className="stat-text">DATA POINT NUMBER TWO</p>
+                <p className="stat-two-number">24</p>
+                <p className="stat-text">pounds of greenhouse gasses emitted with every gallon of gas</p>
             </div>
             <div className="stat-three">
-                <p className="stat-three-number">78</p>
-                <p className="stat-text">DATA POINT NUMBER THREE</p>
+                <p className="stat-three-number">80%</p>
+                <p className="stat-text">of water systmes in America have been found with contaminants linked to cancer</p>
             </div>
         </div>
     )
--- src/components/Tips.js
+++ src/components/Tips.js
@@ -5,28 +5,32 @@ function Tips() {
         <p className="tips-text">Below are just a few tips you can use to work sustainable practices into your every day life.</p>
         <div className="tips-container">
             <div className="tip">
-                <h3 className="tip-headline">TIP #1</h3>
-                <p className="tip-text">TIP NUMBER ONE GOES HERE</p>
+                <h3 className="tip-headline">Use reusable spray bottles</h3>
+                <p className="tip-text">Buy glass spray bottles for household cleaning products that are refillable</p>
             </div>
             <div className="tip" id="two">
-                <h3 className="tip-headline">TIP #2</h3>
-                <p className="tip-text">TIP NUMBER ONE GOES HERE</p>
+                <h3 className="tip-headline">Check ingredients on household products</h3>
+                <p className="tip-text">Looks for biodegradable and non-toxic labels</p>
             </div>
             <div className="tip" id="three">
-                <h3 className="tip-headline">TIP #3</h3>
-                <p className="tip-text">TIP NUMBER ONE GOES HERE</p>
+                <h3 className="tip-headline">Use public transportation when possible</h3>
+                <p className="tip-text">Public transport and other alternatives helps reduce carbon emissions</p>
             </div>
             <div className="tip" id="four">
-                <h3 className="tip-headline">TIP #4</h3>
-                <p className="tip-text">TIP NUMBER ONE GOES HERE</p>
+                <h3 className="tip-headline">Avoid single use waterbottles</h3>
+                <p className="tip-text">Utilize reusable water bottles and tap water (there are great filtrations options out there)</p>
             </div>
             <div className="tip" id="five">
-                <h3 className="tip-headline">TIP #5</h3>
-                <p className="tip-text">TIP NUMBER ONE GOES HERE</p>
+                <h3 className="tip-headline">Bring reusable bags to the store</h3>
+                <p className="tip-text">Bring your own non-single-use bags to the store with you</p>
             </div>
             <div className="tip" id="six">
-                <h3 className="tip-headline">TIP #6</h3>
-                <p className="tip-text">TIP NUMBER ONE GOES HERE</p>
+                <h3 className="tip-headline">Invest in reusable straws</h3>
+                <p className="tip-text">Can't go without a straw? Buy metal/other reusable options to avoid single-use plastic.</p>
+            </div>
+            <div className="tip" id="two">
+                <h3 className="tip-headline">Every little bit helps</h3>
+                <p className="tip-text">Just try and make small changes - they can make a huge difference</p>
             </div>
         </div>
     </div>
