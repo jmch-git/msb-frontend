2e45aa3cd218f366889fb138479297b05b42313c
Nicole Baird <nbaird44@gmail.com>
2022-02-01 20:23:12 -0500

    brand page styling and brand search bar

--- src/pages/Brands.js
+++ src/pages/Brands.js
@@ -1,18 +1,37 @@
 import { Link } from "react-router-dom";
+import { useState } from "react"
 
 function Brands(props) {
+    const [search, setSearch] = useState("");
+
+    function handleChange(e) {
+        setSearch(e.target.value);
+    }
 
     // loaded function
     const loaded = () => {
-        return props.brands.map((brand) => (
-        <div key={brand._id} className="brand-card">
-            <a href={brand.website} target="_blank"><img class="brand-image" src={brand.image} alt={brand.name} /></ a>
-            <h2 className="brand-headline">{brand.name}</h2>
-            <h3 className="brand-subhead">{brand.industry}</h3>
-            <h3 className="brand-details">{brand.description}</h3>
-        </div>
-    ));
-  };
+        return (
+            <>
+            {props.brands
+                .filter((value) => {
+                    if (search === "") {
+                    return value;
+                    } else if (
+                    value.name.toLowerCase().includes(search.toLocaleLowerCase())
+                    ) {
+                    return value;
+                    }
+          }).map((brand) => (
+            <div key={brand._id} className="brand-card">
+                <a href={brand.website} target="_blank"><img class="brand-image" src={brand.image} alt={brand.name} /></ a>
+                <h2 className="brand-headline">{brand.name}</h2>
+                <h3 className="brand-subhead">{brand.industry}</h3>
+                <h3 className="brand-details">{brand.description}</h3>
+            </div>
+        ))}
+        </>
+        );            
+    };
 
   //loading function
   const loading = () => {
@@ -24,6 +43,15 @@ function Brands(props) {
             <div className="intro">
                 <h2 className="tips-headline">Our Brands.</h2>
                 <p className="tips-text">Each of the below brands is recommended by our community. They employ sustainable practices and/or provide sustainable products - making it easier for us to be environmentally conscious.</p>
+                <form className="form">
+                    <label>Search for a brand: </label> <br></br>
+                    <input
+                        placeholder="Just start typing a name..."
+                        name="search"
+                        onChange={handleChange}
+                        id="search-label"
+                    />
+                </form>
             </div>
             <div className="brand-container">
            { props.brands ? loaded() : loading() }
